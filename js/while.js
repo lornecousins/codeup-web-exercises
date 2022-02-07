9944be5f6aba931521d97bd9981af555f3f8ce71
Lorne Cousins <lorne.cousins88@gmail.com>
2022-02-07 15:41:23 -0600

    loops lecture and exercises: while/do-while

---
+++ js/while.js
@@ -0,0 +1,24 @@
+"use strict";
+// While:
+
+// var number = 2;
+// while(number <= 65536) {
+//     console.log(number);
+//     number = number * 2;
+// }
+
+//Do-While:
+
+var allCones = Math.floor(Math.random() * 50) + 50;
+var conesBought;
+do {
+    conesBought = Math.floor(Math.random() * 5) + 1;
+    if (conesBought > allCones) {
+        console.log(("Cannot sell you " + conesBought + " I only have " + allCones));;
+    } else {
+        console.log(conesBought + " cones sold...");
+        allCones = allCones - conesBought;
+    }
+} while(allCones > 0);
+console.log("Yay! I sold them all");
+
