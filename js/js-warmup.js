134caddf95482700d1cbb43cc00e75bd8288bc75
Lorne Cousins <lorne.cousins88@gmail.com>
2022-02-07 12:19:50 -0600

    loops lecture and warm-ups

---
+++ js/js-warmup.js
@@ -0,0 +1,23 @@
+"use strict";
+
+// Write a function that takes in an input and returns the string "I have no idea what you just said";
+
+function whatsThat() {
+    return "I have no idea what you just said";
+}
+
+console.log(whatsThat());
+
+//write a function that takes in a number and returns that number times 2
+// so if you give it 2 it returns 4
+// if you give it 100 it returns 200
+
+function timesTwo(input) {
+    return input * 2;
+}
+
+console.log(timesTwo(2), 4);
+console.log(timesTwo(100), 200  );
+
+
+
---
+++ js/loops-lecture.js
@@ -0,0 +1,39 @@
+// var slices = 4;
+// while(slices > 0) {
+//     console.log("I'll have another slice!");
+//     slices = slices - 1;
+//     console.log("Now there's " + slices + " left");
+// }
+// console.log("Awwww .... no more pizza :(");
+
+// var counter = 0;
+// while(counter <= 100) {
+//     console.log(counter);
+//     // counter = counter + 1;
+//     counter++
+// }
+
+// counter = 100;
+// while(counter >= 88) {
+//     console.log(counter);
+//     counter--;
+//     console.log("Keep on going!");
+// }
+// console.log("I'm finally out of that loop");
+
+// var counter = 1;
+// var number = 5;
+// while(counter <=5) {
+//     number = number * 2;
+//     console.log(number);
+//     counter = counter + 1;
+// }
+
+// var number = 5;
+// while(number < 200) {
+//     console.log(number);
+//     number = number * 2;
+// }
+
+
+
